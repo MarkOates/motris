e3ca868d7ab9ff210113b59acb11da55e0f33d5e
Mark Oates <mark.oates@shopify.com>
2018-05-06 13:14:07 -0400

    Add ability to place figures on a field and get/set tiles

--- include/motris/models/field.hpp
+++ include/motris/models/field.hpp
@@ -1,14 +1,26 @@
 #pragma once
 
 
+#include <motris/models/figure.hpp>
+#include <vector>
+
+
 class Field
 {
 private:
+   const int EMPTY_TILE = 0;
    int width;
    int height;
+   std::vector<int> tiles;
 
 public:
    Field(int width=10, int height=20);
+
+   void set_tile(int x, int y, int tile);
+
+   int get_tile(int x, int y) const;
+
+   bool can_place_figure(const Figure &figure) const;
 };
 
 
--- src/motris/models/field.cpp
+++ src/motris/models/field.cpp
@@ -6,7 +6,34 @@
 Field::Field(int width, int height)
    : width(width)
    , height(height)
+   , tiles(width * height, 0)
 {}
 
+
+void Field::set_tile(int x, int y, int tile)
+{
+   tiles[x + y * width] = tile;
+}
+
+
+int Field::get_tile(int x, int y) const
+{
+   return tiles[x + y * width];
+}
+
+
+bool Field::can_place_figure(const Figure &figure) const
+{
+   for (auto &point : figure.points)
+   {
+      if (point.x < 0) return false;
+      if (point.x >= width) return false;
+      if (point.y >= height) return false;
+
+      if (get_tile(point.x, point.y) != EMPTY_TILE) return false;
+   }
+
+   return true;
+}
 
 
