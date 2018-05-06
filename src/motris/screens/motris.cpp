b575da742cd7bb29839f646157ff4059b2c2504f
Mark Oates <mark.oates@shopify.com>
2018-05-05 22:26:03 -0400

    Add sprite_sheet and bitmap to Motris; Render scaled projection

--- src/motris/screens/motris.cpp
+++ src/motris/screens/motris.cpp
@@ -7,6 +7,8 @@
 
 
 Motris::Motris()
+   : piece_tiles_sprite_sheet(al_load_bitmap("data/bitmaps/piece_tiles.png"), 16, 16)
+   , bitmap(piece_tiles_sprite_sheet.get_sprite(0), 40, 20)
 {
 }
 
@@ -18,7 +20,16 @@ void Motris::update_scene()
 
 void Motris::render_scene()
 {
+   ALLEGRO_BITMAP *backbuffer_bitmap = al_get_backbuffer(al_get_current_display());
+   ALLEGRO_TRANSFORM trans;
+   al_identity_transform(&trans);
+   al_orthographic_transform(&trans, 0, 0, -1.0, al_get_bitmap_width(backbuffer_bitmap)/4, al_get_bitmap_height(backbuffer_bitmap)/4, 1.0);
+   al_set_target_bitmap(backbuffer_bitmap);
+   al_use_projection_transform(&trans);
    al_clear_to_color(color::hex("4b3c37"));
+
+   bitmap.draw();
+
    al_flip_display();
 }
 
