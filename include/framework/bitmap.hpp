6d70d78d565a5e4c3e8c077540649567216a8c2d
Mark Oates <mark.oates@shopify.com>
2018-05-05 23:46:42 -0400

    Move Bitmap code into separate hpp/cpp files

--- include/framework/bitmap.hpp
+++ include/framework/bitmap.hpp
@@ -2,13 +2,7 @@
 
 
 #include <allegro5/allegro.h>
-#include <allegro5/allegro_color.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
 #include <framework/placement2d.hpp>
-#include <framework/color.hpp>
-#include <iostream>
-#include <string>
 
 
 class Bitmap : public placement2d
@@ -20,57 +14,17 @@ public:
    int _flags;
 
 public:
-   Bitmap(ALLEGRO_BITMAP *al_bitmap, float x=0, float y=0)
-      : placement2d(x, y, 0, 0, 0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0)
-      , al_bitmap(al_bitmap)
-      , color(color::white)
-      , opacity(1.0)
-      , _flags(0)
-   {
-      this->bitmap(al_bitmap);
-   }
-   //inline Bitmap &opacity(float opacity) { opacity(opacity); return *this; }
-   //inline Bitmap &save(std::string filename) { al_save_bitmap(filename.c_str(), al_bitmap); return *this; }
-   inline Bitmap &reset()
-   {
-      placement2d::clear();
-      placement2d::align.x = 0.0;
-      placement2d::align.y = 0.0;
-      return *this;
-   }
-   inline Bitmap &flags(int flags) { _flags = flags; return *this; }
-   ALLEGRO_BITMAP *get_bitmap() { return al_bitmap; }
-   inline Bitmap &bitmap(ALLEGRO_BITMAP *bitmap)
-   {
-      al_bitmap = bitmap;
-      if (al_bitmap)
-      {
-         w = al_get_bitmap_width(al_bitmap);
-         h = al_get_bitmap_height(al_bitmap);
-      }
-      else
-      {
-         w = 1;
-         h = 1;
-      }
-      return *this;
-   }
-   inline Bitmap &draw_raw()
-   {
-      if (al_bitmap)
-      {
-         al_draw_tinted_bitmap(al_bitmap, ::color::color(color, opacity), 0, 0, _flags);
-      }
-      return *this;
-   }
-   inline Bitmap &draw()
-   {
-      start_transform();
-      draw_raw();
-      restore_transform();
-      //draw_crosshair(x, y);
-      return *this;
-   }
+   Bitmap(ALLEGRO_BITMAP *al_bitmap, float x=0, float y=0);
+   ~Bitmap();
+
+   //inline Bitmap &opacity(float opacity);
+   //inline Bitmap &save(std::string filename);
+   Bitmap &reset();
+   Bitmap &flags(int flags);
+   ALLEGRO_BITMAP *get_bitmap();
+   Bitmap &bitmap(ALLEGRO_BITMAP *bitmap);
+   Bitmap &draw_raw();
+   Bitmap &draw();
 };
 
 
---
+++ src/framework/bitmap.cpp
@@ -0,0 +1,93 @@
+
+
+#include <framework/bitmap.hpp>
+
+#include <framework/color.hpp>
+
+
+Bitmap::Bitmap(ALLEGRO_BITMAP *al_bitmap, float x, float y)
+   : placement2d(x, y, 0, 0, 0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0)
+   , al_bitmap(al_bitmap)
+   , color(color::white)
+   , opacity(1.0)
+   , _flags(0)
+{
+   this->bitmap(al_bitmap);
+}
+
+
+Bitmap::~Bitmap()
+{
+}
+
+
+//Bitmap &opacity(float opacity)
+//{
+   //opacity(opacity); return *this;
+//}
+
+
+//Bitmap &save(std::string filename)
+//{
+   //al_save_bitmap(filename.c_str(), al_bitmap); return *this;
+//}
+
+
+Bitmap &Bitmap::reset()
+{
+   placement2d::clear();
+   placement2d::align.x = 0.0;
+   placement2d::align.y = 0.0;
+   return *this;
+}
+
+
+Bitmap &Bitmap::flags(int flags)
+{
+   _flags = flags; return *this;
+}
+
+
+ALLEGRO_BITMAP *Bitmap::get_bitmap()
+{
+   return al_bitmap;
+}
+
+
+Bitmap &Bitmap::bitmap(ALLEGRO_BITMAP *bitmap)
+{
+   al_bitmap = bitmap;
+   if (al_bitmap)
+   {
+      w = al_get_bitmap_width(al_bitmap);
+      h = al_get_bitmap_height(al_bitmap);
+   }
+   else
+   {
+      w = 1;
+      h = 1;
+   }
+   return *this;
+}
+
+
+Bitmap &Bitmap::draw_raw()
+{
+   if (al_bitmap)
+   {
+      al_draw_tinted_bitmap(al_bitmap, ::color::color(color, opacity), 0, 0, _flags);
+   }
+   return *this;
+}
+
+
+Bitmap &Bitmap::draw()
+{
+   start_transform();
+   draw_raw();
+   restore_transform();
+   //draw_crosshair(x, y);
+   return *this;
+}
+
+
