a7d4cc54a693808f86f9482188f3eae28323410a
Mark Oates <mark.oates@shopify.com>
2018-05-08 22:21:24 -0400

    Modify TextObject -> Text with more familiar conventions

--- include/framework/text.hpp
+++ include/framework/text.hpp
@@ -1,129 +1,33 @@
-#ifndef __AF_TEXT_OBJECT_HEADER
-#define __AF_TEXT_OBJECT_HEADER
+#pragma once
 
 
-
-
-#include <string>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_color.h>
-#include <allegro_flare/bins/font_bin.h>
-#include <allegro_flare/object2d.h>
-#include <allegro_flare/useful.h>
-#include <allegro_flare/vec2d.h>
+#include <framework/placement2d.hpp>
+#include <string>
 
 
-
-
-class TextObject : public object2d
+class Text
 {
 private:
-   static TextObject *dummy;
+   std::string text;
+   ALLEGRO_COLOR color;
+   ALLEGRO_FONT *font;
 
 public:
-   std::string str;
-   ALLEGRO_FONT *_font;
+   placement2d placement;
 
-   static TextObject *get_dummy()
-   {
-      if (!dummy) dummy = new TextObject("");
-      return dummy;
-   }
+   Text(ALLEGRO_FONT *font, std::string text="", float x=0, float y=0, ALLEGRO_COLOR color=al_map_rgba_f(1,1,1,1));
+   ~Text();
 
-public:
-   TextObject(std::string str="") : object2d(0,0,200,30), str(str), _font(NULL)
-   {
-      text(str);
-   }
+   Text &set_text(std::string text);
+   Text &set_font(ALLEGRO_FONT *font);
+   Text &set_color(ALLEGRO_COLOR color);
 
-   inline TextObject &clean()
-   {
-      if (_placement) _placement->clear();
-      if (_appearance) _appearance->clear();
-      str = "";
-      _font = NULL;
-      *_h = 0;
-      *_w = 0;
-      return *this;
-   }
-
-   inline TextObject &text(std::string str)
-   {
-      if (str == this->str) return *this;
-
-      this->str = str;
-      if (_font)
-      {
-         *_w = al_get_text_width(_font, str.c_str());
-         *_h = al_get_font_line_height(_font);
-      }
-      return *this;
-   }
-
-   inline TextObject &align(float x, float y) override { object2d::align(x, y); return *this; }
-   inline TextObject &position(float x, float y) override { object2d::position(x, y); return *this; }
-   inline TextObject &anchor(float x, float y) override { object2d::anchor(x, y); return *this; }
-   inline TextObject &scale(float x, float y) override { object2d::scale(x, y); return *this; }
-   inline TextObject &scale(float s) override { object2d::scale(s); return *this; }
-   inline TextObject &opacity(float opacity) override { object2d::opacity(opacity); return *this; }
-
-   inline TextObject &font(ALLEGRO_FONT *font)
-   {
-      if (_font == font) return *this;
-      _font = font;
-      *_h = al_get_font_line_height(_font);
-      *_w = al_get_text_width(_font, str.c_str());
-      return *this;
-   }
-
-   inline TextObject &color(ALLEGRO_COLOR _color) override
-   {
-      object2d::color(_color);
-      return *this;
-   }
-
-   inline TextObject &color(const char *_color, float opacity=1.0) override
-   {
-      object2d::color(_color, opacity);
-      return *this;
-   }
-
-   ALLEGRO_BITMAP *create_render()
-   {
-      ALLEGRO_BITMAP *img = NULL;
-      ALLEGRO_STATE state;
-      if (!(*_w>=1.0 && *_h>=1.0)) return NULL;
-
-      img = al_create_bitmap(*_w, *_h);
-      al_store_state(&state, ALLEGRO_STATE_TARGET_BITMAP);
-      al_set_target_bitmap(img);
-      al_clear_to_color(al_map_rgba_f(0,0,0,0));
-      al_draw_text(_font, (_appearance)?color::color(_appearance->color, _appearance->opacity):al_map_rgba_f(1,1,1,1), 0, 0, ALLEGRO_FLAGS_EMPTY, str.c_str());
-      al_restore_state(&state);
-      return img;
-   }
-
-   inline TextObject &draw() override
-   {
-      if (_placement) _placement->start_transform();
-      if (_font) al_draw_text(_font, (_appearance)?color::color(_appearance->color, _appearance->opacity):al_map_rgba_f(1,1,1,1), 0, 0, ALLEGRO_FLAGS_EMPTY, str.c_str());
-      if (_placement) _placement->restore_transform();
-      return *this;
-   }
+   Text &set_placement_size_to_text();
+   Text &clean();
+   Text &draw();
 };
 
 
-
-
-inline TextObject &text(std::string str)
-{
-   TextObject::get_dummy()->clean().text(str);
-   return *TextObject::get_dummy();
-}
-
-
-
-
-#endif
--- src/framework/text.cpp
+++ src/framework/text.cpp
@@ -1,14 +1,78 @@
 
 
+#include <framework/text.hpp>
 
 
-#include <allegro_flare/objects/text_object.h>
+Text::Text(ALLEGRO_FONT *font, std::string text, float x, float y, ALLEGRO_COLOR color)
+   : text(text)
+   , color(color)
+   , font(font)
+   , placement(x, y, 0, 0)
+{
+   set_text(text);
+   set_placement_size_to_text();
+}
 
 
+Text::~Text()
+{
+}
 
 
-TextObject *TextObject::dummy = NULL;
+Text &Text::set_text(std::string text)
+{
+   this->text = text;
+   return *this;
+}
 
 
+Text &Text::set_font(ALLEGRO_FONT *font)
+{
+   this->font = font;
+   return *this;
+}
+
+
+Text &Text::set_color(ALLEGRO_COLOR color)
+{
+   this->color = color;
+   return *this;
+}
+
+
+Text &Text::set_placement_size_to_text()
+{
+   if (font)
+   {
+      placement.size.x = al_get_text_width(font, text.c_str());
+      placement.size.y = al_get_font_line_height(font);
+   }
+   else
+   {
+      placement.size = vec2d(0, 0);
+   }
+   return *this;
+}
+
+
+Text &Text::clean()
+{
+   placement.clear();
+   text = "";
+   font = NULL;
+   return *this;
+}
+
+
+Text &Text::draw()
+{
+   if (!font) std::runtime_error("Cannot render text, font is nullptr");
+
+   placement.start_transform();
+   al_draw_text(font, color, 0, 0, 0, text.c_str());
+   placement.restore_transform();
+
+   return *this;
+}
 
 
