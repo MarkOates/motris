c4b5d45eac2f0e75298a0ab01e99b4f5cdee5a62
Mark Oates <mark.oates@shopify.com>
2018-05-09 00:18:48 -0400

    Add GAME_EVENT_START_GAMEPLAY; Emit on Motris() constructor

--- src/motris/screens/motris.cpp
+++ src/motris/screens/motris.cpp
@@ -2,18 +2,24 @@
 
 #include <motris/screens/motris.hpp>
 
+#include <motris/screens/gameplay.hpp>
 #include <motris/events.hpp>
 
 
 Motris::Motris(Framework &framework)
    : framework(framework)
-{}
+{
+   emit_event(GAME_EVENT_START_GAMEPLAY);
+}
 
 
 void Motris::process_event(ALLEGRO_EVENT &event)
 {
    switch(event.type)
    {
+   case GAME_EVENT_START_GAMEPLAY:
+      framework.add_screen(new Gameplay);
+      break;
    case ALLEGRO_EVENT_DISPLAY_CLOSE:
       emit_event(EVENT_ABORT_PROGRAM);
       break;
