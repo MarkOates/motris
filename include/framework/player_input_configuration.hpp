9f563422b305c92404898c21dc1c103b89aebe31
Mark Oates <mark.oates@shopify.com>
2018-05-12 21:10:51 -0400

    Add PlayerInputConfiguration and player_input_command_t

--- include/framework/player_input_configuration.hpp
+++ include/framework/player_input_configuration.hpp
@@ -1,43 +1,35 @@
 #pragma once
 
 
+#include <framework/player_input_commands.hpp>
 #include <map>
-
-
-enum button_t
-{
-   GAMER_BUTTON_UNDEF = 0,
-   GAMER_BUTTON_UP,
-   GAMER_BUTTON_DOWN,
-   GAMER_BUTTON_LEFT,
-   GAMER_BUTTON_RIGHT,
-   GAMER_BUTTON_START,
-   GAMER_BUTTON_BACK,
-   GAMER_BUTTON_A,
-   GAMER_BUTTON_B,
-   GAMER_BUTTON_C,
-   GAMER_BUTTON_SIZE_MAX,
-};
 
 
 class PlayerInputConfiguration
 {
 private:
    int joystick_num;
-   std::map<button_t, int> joystick_mappings;
-   std::map<button_t, int> keyboard_mappings;
+   bool use_joystick_axes_for_arrow_commands;
+   std::map<int, player_input_command_t> joystick_mappings;
+   std::map<int, player_input_command_t> keyboard_mappings;
 
 public:
    PlayerInputConfiguration();
    ~PlayerInputConfiguration();
 
    void set_joystick_num(int joystick_num);
-   void add_joystic_mapping(button_t button, int joy_button_num);
-   void add_keyboard_mapping(button_t button, int allegro_keycode);
+   //void add_keyboard_mapping(player_input_command_t button, int al_keycode);
+   //void add_joystic_mapping(player_input_command_t button, int joystick_button_num);
+   void set_use_joystick_axes_for_arrow_commands(bool use=true);
+
+   bool is_usiing_joystick_axes_for_arrow_commands();
+
+   bool is_joystick_num(int num);
+   player_input_command_t get_keyboard_mapping(int al_keycode);
+   player_input_command_t get_joystick_mapping(int joystick_button_num);
 
    void setup_default_keyboard_mapping_for_ARROW_KEYS();
    void setup_default_joystick_mapping_for_XBOX_360_CONTROLLER();
-   void setup_default_joystick_mapping_for_LOGITECH_RUMBLEPAD_2();
 };
 
 
--- src/framework/player_input_configuration.cpp
+++ src/framework/player_input_configuration.cpp
@@ -2,9 +2,11 @@
 
 #include <framework/player_input_configuration.hpp>
 
+#include <allegro5/allegro.h>
+
 
 PlayerInputConfiguration::PlayerInputConfiguration()
-   : joystick_num(0)
+   : joystick_num(-1)
    , joystick_mappings()
    , keyboard_mappings()
 {
@@ -16,46 +18,78 @@ PlayerInputConfiguration::~PlayerInputConfiguration()
 }
 
 
-void PlayerInputConfiguration::setup_default_keyboard_mapping_for_ARROW_KEYS()
+void PlayerInputConfiguration::set_joystick_num(int joystick_num)
 {
-   keyboard_mappings.clear();
-   //button_up_keyboard_keymap = ALLEGRO_KEY_UP;
-   //button_down_keyboard_keymap = ALLEGRO_KEY_DOWN;
-   //button_left_keyboard_keymap = ALLEGRO_KEY_LEFT;
-   //button_right_keyboard_keymap = ALLEGRO_KEY_RIGHT;
-   //button_start_keyboard_keymap = ALLEGRO_KEY_ENTER;
-   //button_back_keyboard_keymap = ALLEGRO_KEY_BACKSPACE;
-   //button_a_keyboard_keymap = ALLEGRO_KEY_Z;
-   //button_b_keyboard_keymap = ALLEGRO_KEY_SPACE;
-   //button_c_keyboard_keymap = ALLEGRO_KEY_X;
+   this->joystick_num = joystick_num;
 }
 
 
-void PlayerInputConfiguration::setup_default_joystick_mapping_for_LOGITECH_RUMBLEPAD_2()
+void PlayerInputConfiguration::set_use_joystick_axes_for_arrow_commands(bool use)
 {
-   //button_up_joystick_buttonmap = 999;
-   //button_down_joystick_buttonmap = 999;
-   //button_left_joystick_buttonmap = 999;
-   //button_right_joystick_buttonmap = 999;
-   //button_start_joystick_buttonmap = 9;
-   //button_back_joystick_buttonmap = 8;
-   //button_a_joystick_buttonmap = 0;
-   //button_b_joystick_buttonmap = 1;
-   //button_c_joystick_buttonmap = 2;
+   use_joystick_axes_for_arrow_commands = use;
+}
+
+
+bool PlayerInputConfiguration::is_usiing_joystick_axes_for_arrow_commands()
+{
+   return use_joystick_axes_for_arrow_commands;
+}
+
+
+bool PlayerInputConfiguration::is_joystick_num(int num)
+{
+   return this->joystick_num == num;
+}
+
+
+player_input_command_t PlayerInputConfiguration::get_keyboard_mapping(int al_keycode)
+{
+   std::map<int, player_input_command_t>::iterator it = keyboard_mappings.find(al_keycode);
+   if (it == keyboard_mappings.end()) return PLAYER_INPUT_COMMAND_UNDEF;
+   return it->second;
+}
+
+
+player_input_command_t PlayerInputConfiguration::get_joystick_mapping(int joystick_button_num)
+{
+   std::map<int, player_input_command_t>::iterator it = joystick_mappings.find(joystick_button_num);
+   if (it == joystick_mappings.end()) return PLAYER_INPUT_COMMAND_UNDEF;
+   return it->second;
+}
+
+
+void PlayerInputConfiguration::setup_default_keyboard_mapping_for_ARROW_KEYS()
+{
+   keyboard_mappings.clear();
+
+   keyboard_mappings[ALLEGRO_KEY_UP] = PLAYER_INPUT_COMMAND_UP;
+   keyboard_mappings[ALLEGRO_KEY_DOWN] = PLAYER_INPUT_COMMAND_DOWN;
+   keyboard_mappings[ALLEGRO_KEY_LEFT] = PLAYER_INPUT_COMMAND_LEFT;
+   keyboard_mappings[ALLEGRO_KEY_RIGHT] = PLAYER_INPUT_COMMAND_RIGHT;
+
+   keyboard_mappings[ALLEGRO_KEY_ENTER] = PLAYER_INPUT_COMMAND_START;
+   keyboard_mappings[ALLEGRO_KEY_BACKSPACE] = PLAYER_INPUT_COMMAND_BACK;
+   keyboard_mappings[ALLEGRO_KEY_Z] = PLAYER_INPUT_COMMAND_A;
+   keyboard_mappings[ALLEGRO_KEY_SPACE] = PLAYER_INPUT_COMMAND_B;
+   keyboard_mappings[ALLEGRO_KEY_X] = PLAYER_INPUT_COMMAND_C;
 }
 
 
 void PlayerInputConfiguration::setup_default_joystick_mapping_for_XBOX_360_CONTROLLER()
 {
-   //button_up_joystick_buttonmap = 999;
-   //button_down_joystick_buttonmap = 999;
-   //button_left_joystick_buttonmap = 999;
-   //button_right_joystick_buttonmap = 999;
-   //button_start_joystick_buttonmap = 11;
-   //button_back_joystick_buttonmap = 6;
-   //button_a_joystick_buttonmap = 3;
-   //button_b_joystick_buttonmap = 0;
-   //button_c_joystick_buttonmap = 1;
+   joystick_mappings.clear();
+
+   // there are no mappings for up/down/left/right
+   //keyboard_mappings[PLAYER_INPUT_COMMAND_UP] = 999;
+   //keyboard_mappings[PLAYER_INPUT_COMMAND_DOWN] = 999;
+   //keyboard_mappings[PLAYER_INPUT_COMMAND_LEFT] = 999;
+   //keyboard_mappings[PLAYER_INPUT_COMMAND_RIGHT] = 999;
+
+   keyboard_mappings[11] = PLAYER_INPUT_COMMAND_START;
+   keyboard_mappings[6] = PLAYER_INPUT_COMMAND_BACK;
+   keyboard_mappings[3] = PLAYER_INPUT_COMMAND_A;
+   keyboard_mappings[0] = PLAYER_INPUT_COMMAND_B;
+   keyboard_mappings[1] = PLAYER_INPUT_COMMAND_C;
 }
 
 
